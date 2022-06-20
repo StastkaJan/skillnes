a0f82e571d36ab0f232f219d07c1b9822e0f7084
Jan Stastka <staj19@vse.cz>
2022-06-20 16:31:28 +0200

    Basic setup and layout. Commit v2.

---
+++ front/src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        message: error.message,
+        status
+      }
+    }
+  }
+</script>
+
+<script>
+  import ErrorScreen from '../screens/errors/e.svelte' // your own Error screen component
+  import NotFoundScreen from '../screens/errors/e404.svelte' // your own 404 screen component
+
+  export let message, status
+</script>
+
+{#if status == 404}
+  <NotFoundScreen />
+{:else}
+  <ErrorScreen {message} {status} />
+{/if}
---
+++ front/src/screens/errors/e.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let message, status
+</script>
+
+<h1>General error</h1>
+<p>{message}</p>
+<p>{status}</p>
